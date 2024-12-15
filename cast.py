cb08b603f00e92c42d49b2ff5a0661b733644100
Jisheng Jiang <jiangjs03@gmail.com>
2024-12-15 14:32:30 -0800

    input.py need continue work

---
+++ cast.py
@@ -0,0 +1,24 @@
+# Casting data types
+
+name = "Ian"
+age = 21
+gpa = 3.14
+student = True
+
+# age = float(age)
+# print(age)
+# print(type(age))
+#
+# gpa = str(gpa)
+# print(gpa)
+# print(type(gpa))
+
+# print(student)
+# student = str(student)
+# print(student)
+# print(type(student))
+
+# auto casting
+# x = 10
+# y = 2.0
+# print(x / y)
---
+++ input.py
@@ -0,0 +1,17 @@
+# input
+# name = input("What is your name? ")
+# print(f"Hello {name}")
+from curses.textpad import rectangle
+
+# words filling
+# word_1 = input("Enter word 1: ")
+# word_2 = input("Enter word 2: ")
+# word_3 = input("Enter word 3: ")
+# print(f"{word_1} {word_2} {word_3}")
+
+# calculate rectangle This is place that cant work since `curse` module does not support Windows
+# but it does can work on linux and Mac please retry this when you trans different devices
+# height = int(input("Enter height: "))
+# width = int(input("Enter width: "))
+# area = height * width
+# print(f"Area is {area}")
