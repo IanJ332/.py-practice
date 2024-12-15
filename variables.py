74c8d470db94c805dd7401307cc3e44eb5a68e9f
Jisheng Jiang <jiangjs03@gmail.com>
2024-12-15 13:44:20 -0800

    variables.py update

---
+++ variables.py
@@ -0,0 +1,29 @@
+# print('Hello World!')
+from tokenize import String
+
+# x = 1
+# y = 2
+# print(x+y)
+
+# x = 'Hello there'
+# print(x)
+
+# age = 21
+# print('My age is ' + str(age)) #trans
+
+#integer
+# age = 21
+#
+# # float
+# GPA = 3.64
+#
+# print(F'My GPA is {GPA}')
+
+#String
+# name = 'Ian'
+# print(f'Hello {name}')
+
+# boolean
+# is_21 = True
+# print(type(is_21)) type() will show the type of the data
+
