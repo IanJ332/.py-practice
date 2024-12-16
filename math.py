2a21b0f7b5e8f4891d8db5e7952f53e086425e22
IanJ332 <jiangjs03@gmail.com>
2024-12-16 13:45:25 -0800

    math been done

---
+++ math.py
@@ -0,0 +1,41 @@
+# apple = 1
+# item = 1
+#
+# apple += 1
+# print(apple)
+#
+# apple -= 1
+# print(apple)
+#
+# apple = 10
+# print(int(apple / 2))
+#
+# print(int(apple % 3))
+#
+# print(pow(apple, 2))
+#
+# print(max(apple, 666))
+# print(min(apple, 666))
+import math
+from string import digits
+
+# digits = 3.3
+# digits2 = 3.9
+#
+# print(int(digits))
+# print(int(digits2))
+#
+# print(round(digits))
+# print(round(digits2))
+
+# digits = -1.1
+# print(digits)
+# print(abs(digits))
+
+# Round is same like ceil, and if you cast like int(1.1), is same as floor
+# digits = 1.5
+# print(math.ceil(digits))
+# print(math.floor(digits)
+
+# radius = float(input("Enter radius: "))
+# print(f"the area is {math.pi * pow(radius, 2)}")
