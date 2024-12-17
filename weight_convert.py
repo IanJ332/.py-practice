da39a9c5fd6d465d1b4e3d530d14e8abdfe4b693
IanJ332 <jiangjs03@gmail.com>
2024-12-16 16:45:33 -0800

    conterter.py

---
+++ weight_convert.py
@@ -0,0 +1,9 @@
+weight = int(input('Enter weight in pounds: '))
+units = input('Enter (l)bs or (k)g: ').upper()
+
+if units == 'KG':
+    print(f'Your weight is {weight}, convert to pounds is {weight * 2.20462} pounds')
+elif units == 'LB':
+    print(f'Your weight is {weight}, convert to kilograms is {weight / 2.20462} kilograms')
+else:
+    print('Invalid input, please enter lbs or kg')
