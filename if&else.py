4c7f4cf4f5c0ec2584476c078102e6ef612e409f
IanJ332 <jiangjs03@gmail.com>
2024-12-16 16:07:18 -0800

    symbol calculator

--- if&else.py
+++ if&else.py
@@ -1,11 +1,22 @@
-age = int(input("Whats your age?"))
-isAdult= True
-if age >= 18:
-    isAdult = True
-else:
-    isAdult = False
+# age = int(input("Whats your age?"))
+# isAdult= True
+# if age >= 18:
+#     isAdult = True
+# else:
+#     isAdult = False
+#
+# if isAdult:
+#     print("You are adults")
+# else:
+#     print("You are not adults")
 
-if isAdult:
-    print("You are adults")
-else:
-    print("You are not adults")
+num1 = float(input("Whats first number?"))
+num2 = float(input("Whats second number?"))
+
+symbol = str(input("What operation you would like to do?"))
+if symbol == "+": print(f"the sum is {int(num2 + num1)}")
+elif symbol == "-": print(f"the difference is {num1 - num2}")
+elif symbol == "x" or symbol == "*": print(f"the great is {num1 * num2}")
+elif symbol == "/": print(f"the division is {num1 / num2}")
+
+else: print("pleas enter valid operation symbol: add, sub, multi, div")
