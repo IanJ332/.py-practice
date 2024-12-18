7de344d7a0db77170ecaf0c618bd584cb971eda9
Jisheng Jiang <jiangjs03@gmail.com>
2024-12-18 14:34:48 -0800

    tired day

--- list&sets&tuple.py
+++ list&sets&tuple.py
@@ -1,4 +1,31 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 2,2,3,4,5]
+# List
+# Ordered: Elements have a specific order, and this order is maintained.
+#
+# Mutable: Elements can be added, removed, or changed.
+#
+# Allows Duplicates: Elements can appear more than once.
+#
+# Indexable: Elements can be accessed by their index.
+
+# Set
+# Unordered: Elements have no specific order.
+#
+# Mutable: Elements can be added or removed.
+#
+# No Duplicates: Each element is unique.
+#
+# Not Indexable: Elements cannot be accessed by index.
+#
+# Tuple
+# Ordered: Elements have a specific order.
+#
+# Immutable: Elements cannot be changed after the tuple is created.
+#
+# Allows Duplicates: Elements can appear more than once.
+#
+# Indexable: Elements can be accessed by their index.
+
+'# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 2,2,3,4,5]
 
 # add number into the list, and its allow duplicate elements exists
 # print(numbers.append(2))
@@ -15,6 +42,16 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 2,2,3,4,5]
 # print(numbers.index(2))
 
 # reverse the list
-print(numbers)
-numbers.reverse()
-print(numbers)
+# print(numbers)
+# numbers.reverse()
+# print(numbers)
+
+# set is ordered, and not duplicate but you cant use index
+# set = {2, 4, 1, 5, 6, 8, 7 , 10}
+# for numbers in set:
+#     print(numbers, end=" ")
+
+# Define the tuple
+my_tuple = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+print(my_tuple.count('1'))
+
