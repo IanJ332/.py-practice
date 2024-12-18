a58177a592fb48128db79e8c76373d1eb2fac32f
Jisheng Jiang <jiangjs03@gmail.com>
2024-12-18 11:33:46 -0800

    list

---
+++ list&sets&tuple.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 2,2,3,4,5]
+
+# add number into the list, and its allow duplicate elements exists
+# print(numbers.append(2))
+# print(numbers))
+
+# remove
+# print(numbers.remove(2))
+# print(numbers)
+
+# count how many elements in the list
+# print(numbers.count(2))
+
+# find
+# print(numbers.index(2))
+
+# reverse the list
+print(numbers)
+numbers.reverse()
+print(numbers)
