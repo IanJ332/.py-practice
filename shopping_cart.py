156b9a55fc1a29e861aed6fa453a96ce5c3ef369
Jisheng Jiang <jiangjs03@gmail.com>
2024-12-20 15:28:29 -0800

    part done

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,20 +1,18 @@
 goods = []
 prices = []
 
-
 while True:
     good = input('What is the product you would like to buy?')
-    if good.lower == 'q':
-        print('Please enter a product name.')
+    if good.lower() == 'q':  # Corrected here
+        print('You have exited the input process.')
         break
 
     price = float(input(f'What is the price of the {good}?'))
     goods.append(good)
     prices.append(price)
 
+# Print the goods and prices
 for index, good in enumerate(goods):
-    print(index, good)
-    print(good)
+    print(f'{index}: {good}')
 for index, price in enumerate(prices):
-    print(index, price)
-    print(price)
+    print(f'{index}: {price}')
